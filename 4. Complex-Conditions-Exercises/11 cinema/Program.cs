fbaedab3273c43e092e948f4a85230ac25e60948
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-29 22:44:35 +0200

    Add files via upload

---
+++ 4. Complex-Conditions-Exercises/11 cinema/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11cinema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string type = Console.ReadLine();
+            double rows = double.Parse(Console.ReadLine());
+            double cols = double.Parse(Console.ReadLine());
+
+            double numSeats = rows * cols;
+            double result = 0;
+
+            switch (type)
+            {
+                case "Premiere":
+                    result = numSeats * 12;
+                    break;
+                case "Normal":
+                    result = numSeats * 7.50;
+                    break;
+                case "Discount":
+                    result = numSeats * 5;
+                    break;
+            }
+            Console.WriteLine($"{result:f2} leva");
+        }
+    }
+}
