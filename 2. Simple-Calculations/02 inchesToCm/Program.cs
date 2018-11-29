2739982f6894938f8a8e64a2b2ff2855f7f35628
yotkoKanchev <yotko.kanchev@gmail.com>
2018-11-29 19:17:15 +0200

    update name

---
+++ 2. Simple-Calculations/02 inchesToCm/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inchesToCm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("inches = ");
+            double inches = double.Parse(Console.ReadLine());
+
+            double inCm = inches * 2.54;
+
+            Console.Write("Centimeters = ");
+            Console.WriteLine(inCm);
+        }
+    }
+}
