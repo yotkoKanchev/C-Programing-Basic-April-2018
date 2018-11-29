f1e01156cd72b85cd6dcc7838f650212c80da9ef
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-29 19:25:27 +0200

    Add files via upload

---
+++ 2. Simple-Calculations/08 triangleArea/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace triangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a = float.Parse(Console.ReadLine());
+            float h = float.Parse(Console.ReadLine());
+
+            float area = a * h / 2;
+            Console.WriteLine($"Triangle area = {Math.Round(area, 2)}");
+            
+        }
+    }
+}
