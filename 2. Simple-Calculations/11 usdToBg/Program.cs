b44f269198def2cc4a6852aa364989bb51414774
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-29 22:27:08 +0200

    Add files via upload

---
+++ 2. Simple-Calculations/11 usdToBg/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace usdToBg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double usd = double.Parse(Console.ReadLine());
+
+            double bgn = usd * 1.79549;
+
+            Console.WriteLine($"{bgn:f2} BGN");
+        }
+    }
+}
