c7092eabab96c6fc485c3a135364d9446bc3f373
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-29 16:20:15 +0200

    Add files via upload

---
+++ 3. Simple-Conditions/10 Numbers 100 to 200/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace number100To200
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputNumber = int.Parse(Console.ReadLine());
+
+            if (inputNumber < 100)
+            {
+                Console.WriteLine("Less than 100");
+            }
+
+            else if (inputNumber > 200)
+            {
+                Console.WriteLine("Greater than 200");
+            }
+
+            else
+                Console.WriteLine("Between 100 and 200");
+        }
+    }
+}
