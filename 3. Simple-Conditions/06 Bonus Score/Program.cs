cf660b32a078d23989099267f3b5d3f0475489da
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-29 16:17:44 +0200

    Add files via upload

---
+++ 3. Simple-Conditions/06 Bonus Score/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bonusScore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            double bonus = 0;
+
+            if (num <= 100)
+            {
+                bonus = 5;
+            }
+
+            else if (num > 1000)
+            {
+                bonus = num * 0.1;
+            }
+
+            else if (num > 100)
+            {
+                bonus = num * 0.2;
+            }
+
+            if (num % 2 == 0)
+            {
+                bonus += 1;
+            }
+
+            if (num %10 ==5)
+            {
+                bonus += 2;
+            }
+
+            Console.WriteLine(bonus);
+            Console.WriteLine(num + bonus);
+        }   
+    }
+}
