1dad49a02cbcba61accee9d6811c3b0f19f782b5
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-29 23:06:56 +0200

    Add files via upload

---
+++ 6. Drawing-with-Loops/05 Square Frame/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Square_Frame2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int numSymb = n - 2;
+            string symb = " -";
+            string newSym = string.Join(symb, new string[numSymb + 1]);
+
+            Console.WriteLine('+' + newSym + ' ' + '+');
+
+            for (int i = 0; i < n - 2; i++)
+            {
+                Console.WriteLine('|' + newSym + ' ' + '|');
+            }
+
+            Console.WriteLine('+' + newSym + ' ' + '+');
+        }
+    }
+}
