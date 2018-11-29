63db73f884d28c347b7ad17f426e98f005bb7e39
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-29 18:05:44 +0200

    Add files via upload

---
+++ 5. Loops/08 oddEvenSum/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08oddEvenSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int oddSum = 0;
+            int evenSum = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                if (i % 2 == 0)
+                {
+                    evenSum += number;
+                }
+                else
+                {
+                    oddSum += number;
+                }
+            }
+            if (evenSum == oddSum)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {evenSum}");
+            }
+            else
+            {
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {Math.Abs(oddSum-evenSum)}");
+            }
+        }
+    }
+}
