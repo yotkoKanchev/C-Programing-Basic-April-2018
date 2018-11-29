f9cdd2cc81a1815f941f5f53f2529c518c8a1926
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-29 19:26:20 +0200

    Add files via upload

---
+++ 2. Simple-Calculations/09 celsiusToFahrenheit/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace celsiusToFahrenheit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double celsius = double.Parse(Console.ReadLine());
+            double pharenheit = celsius * 1.8 + 32;
+            Console.WriteLine(pharenheit);
+        }
+    }
+}
