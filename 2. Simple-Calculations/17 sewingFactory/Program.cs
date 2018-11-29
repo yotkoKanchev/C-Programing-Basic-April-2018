d7520295607c71668850e0e924c280c6f14b30e4
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-29 22:31:39 +0200

    Add files via upload

---
+++ 2. Simple-Calculations/17 sewingFactory/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15sewingFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double tables = double.Parse(Console.ReadLine());
+            double lenght = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+
+            double totalMoneySquares = tables * (lenght / 2) * (lenght / 2) ;
+            double totalMoneyRectangles = tables * (lenght + 0.6) * (width + 0.6);
+            double totalMoney = (totalMoneyRectangles * 7) + (9 * totalMoneySquares);
+            Console.WriteLine($"{totalMoney:f2} USD");
+            Console.WriteLine($"{totalMoney*1.85:f2} BGN");
+        }
+    }
+}
