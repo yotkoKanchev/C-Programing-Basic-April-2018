3968efadb6f177c422564eea6d7788e390c1e2e1
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-29 23:11:54 +0200

    Add files via upload

---
+++ 6. Drawing-with-Loops/10 Diamond/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diamond1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int leftRightDashes = (n - 1) / 2;
+            int midDashes = n - 2 * leftRightDashes - 2;
+
+            if (n == 1)
+            {
+                Console.WriteLine("*");
+            }
+
+            else
+            {
+                if (midDashes < 0)
+                {
+                    midDashes = 1;
+                    Console.WriteLine((new string('-', leftRightDashes)) + '*' + new string('-', leftRightDashes));
+                    Console.WriteLine((new string('-', leftRightDashes - 1)) + '*' + new string('-', midDashes) + '*' + new string('-', leftRightDashes - 1));
+                    for (int i = 0; i < n / 2 - 1; i++)
+                    {
+                        leftRightDashes--;
+                        midDashes += 2;
+                        Console.WriteLine((new string('-', leftRightDashes - 1)) + '*' + new string('-', midDashes) + '*' + new string('-', leftRightDashes - 1));
+                    }
+                    for (int i = 0; i < n / 2 - 1; i++)
+                    {
+                        leftRightDashes++;
+                        midDashes -= 2;
+                        Console.WriteLine((new string('-', leftRightDashes - 1)) + '*' + new string('-', midDashes) + '*' + new string('-', leftRightDashes - 1));
+                    }
+                    Console.WriteLine((new string('-', leftRightDashes)) + '*' + new string('-', leftRightDashes));
+
+                }
+
+                else
+                {
+                    Console.WriteLine((new string('-', leftRightDashes)) + '*' + new string('-', midDashes) + '*' + new string('-', leftRightDashes));
+                    for (int i = 0; i < n / 2 - 1; i++)
+                    {
+                        leftRightDashes--;
+                        midDashes += 2;
+                        Console.WriteLine((new string('-', leftRightDashes)) + '*' + new string('-', midDashes) + '*' + new string('-', leftRightDashes));
+                    }
+                    for (int i = 0; i < n / 2 - 1; i++)
+                    {
+                        leftRightDashes++;
+                        midDashes -= 2;
+                        Console.WriteLine((new string('-', leftRightDashes)) + '*' + new string('-', midDashes) + '*' + new string('-', leftRightDashes));
+                    }
+                }
+            }
+        }
+    }
+}
