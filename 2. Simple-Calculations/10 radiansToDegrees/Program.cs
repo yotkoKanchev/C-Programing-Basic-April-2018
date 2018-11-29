34a287d5223b4136bb631dc4f8019151118764b7
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-29 19:27:01 +0200

    Add files via upload

---
+++ 2. Simple-Calculations/10 radiansToDegrees/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace radiansToDegrees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radians = double.Parse(Console.ReadLine());
+            double degrees = radians * 180 / Math.PI;
+
+            Console.WriteLine(Math.Round(degrees));
+        }
+    }
+}
