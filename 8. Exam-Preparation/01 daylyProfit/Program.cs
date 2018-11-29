26d80a0ad6d92f555a13b282bbbd46163d22a2b5
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-29 22:51:53 +0200

    Add files via upload

---
+++ 8. Exam-Preparation/01 daylyProfit/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01daylyProfit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double workDays = double.Parse(Console.ReadLine());
+            double moneyPerDay = double.Parse(Console.ReadLine());
+            double exchangeRateUsdBgn = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{((workDays * moneyPerDay * 14.5 * 0.75 * exchangeRateUsdBgn) / 365):f2}");
+
+
+        }
+    }
+}
