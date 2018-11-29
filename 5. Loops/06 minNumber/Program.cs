5301c71485fd21b4e116ca77cdb337524e9d5b54
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-29 18:04:39 +0200

    Add files via upload

---
+++ 5. Loops/06 minNumber/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05maxNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int minNum = 10000000;
+            for (int i = 1; i <= n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                if (number <= minNum)
+                {
+                    minNum = number;
+                }
+            }
+            Console.WriteLine(minNum);
+        }
+    }
+}
