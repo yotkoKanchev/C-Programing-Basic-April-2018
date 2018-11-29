4f27035d5d93cb87eec3576e6e5ac1e0fe86be2d
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-29 22:40:45 +0200

    Add files via upload

---
+++ 4. Complex-Conditions-Exercises/03 pointInRectangle/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03pointInRectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x1 = double.Parse(Console.ReadLine());
+            double y1 = double.Parse(Console.ReadLine());
+            double x2 = double.Parse(Console.ReadLine());
+            double y2 = double.Parse(Console.ReadLine());
+            double x = double.Parse(Console.ReadLine());
+            double y = double.Parse(Console.ReadLine());
+            string result = "";
+            if (x >= x1 && x <= x2 && y >= y1 && y <= y2)
+            {
+                result = "Inside";
+            }
+            else
+            {
+                result = "Outside";
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
