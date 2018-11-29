3c1759a65534e78defcf9a698ad24f4d00f9bfaf
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-29 18:08:14 +0200

    Add files via upload

---
+++ 5. Loops/19 histogram/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19histogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n = double.Parse(Console.ReadLine());
+
+            double p1 = 0;
+            double p2 = 0;
+            double p3 = 0;
+            double p4 = 0;
+            double p5 = 0;
+
+            for (double i = 1; i <= n; i++)
+            {
+                double num = double.Parse(Console.ReadLine());
+                if (num < 200)
+                {
+                    p1 += 1;
+                }
+                else if (num < 400)
+                {
+                    p2 += 1;
+                }
+                else if (num < 600)
+                {
+                    p3 += 1;
+                }
+                else if (num < 800)
+                {
+                    p4 += 1;
+                }
+                else if (num >= 800)
+                {
+                    p5 += 1;
+                }
+            }
+            Console.WriteLine($"{(100/n * p1):f2}%");
+            Console.WriteLine($"{(100/n * p2):f2}%");
+            Console.WriteLine($"{(100/n * p3):f2}%");
+            Console.WriteLine($"{(100/n * p4):f2}%");
+            Console.WriteLine($"{(100/n * p5):f2}%");
+        }
+    }
+}
