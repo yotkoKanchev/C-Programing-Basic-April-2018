bcc30249263872e29045e8972eb5fd2a14f35740
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-29 23:10:40 +0200

    Add files via upload

---
+++ 6. Drawing-with-Loops/08 Sunglasses/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sunglasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(new string('*', n * 2) + new string(' ', n) + new string('*', n * 2));
+
+            for (int i = 0; i < n - 2; i++)
+            {
+                Console.Write("*");
+                Console.Write(new string('/', n * 2 - 2));
+                Console.Write("*");
+
+                if (i == (n - 1) / 2 - 1)
+                {
+                    Console.Write(new string('|', n));
+                }
+
+                else
+                {
+                    Console.Write(new string(' ', n));
+                }
+            }
+
+            for (int i = 0; i < n - 2; i++)
+            {
+                Console.Write("*");
+                Console.Write(new string('/', n * 2 - 2));
+                Console.Write("*");
+                Console.WriteLine();
+            }
+            
+            Console.WriteLine(new string('*', n * 2) + new string(' ', n) + new string('*', n * 2));
+        }
+    }
+}
