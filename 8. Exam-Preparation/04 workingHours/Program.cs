b341109958156c78a4dc47cbf7aa95fd9e5b7445
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-29 22:53:25 +0200

    Add files via upload

---
+++ 8. Exam-Preparation/04 workingHours/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04workingHours
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double hoursNeeded = double.Parse(Console.ReadLine());
+            double workersNumber = double.Parse(Console.ReadLine());
+            double workingDays = double.Parse(Console.ReadLine());
+
+            double workingHours = workersNumber * workingDays * 8;
+
+            double diff = Math.Abs(hoursNeeded - workingHours);
+
+            if (hoursNeeded<= workingHours)
+            {
+                Console.WriteLine($"{diff} hours left");
+            }
+            else
+            {
+                Console.WriteLine($"{diff} overime");
+                Console.WriteLine($"Penalties: {diff * workingDays}");
+            }
+
+        }
+    }
+}
