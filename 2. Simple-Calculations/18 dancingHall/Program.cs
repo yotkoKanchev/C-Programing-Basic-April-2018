65d112387b6433ab0b5f2e858d026bf019cc2ea2
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-29 22:32:12 +0200

    Add files via upload

---
+++ 2. Simple-Calculations/18 dancingHall/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18dancingHall
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double l = double.Parse(Console.ReadLine());
+            double w = double.Parse(Console.ReadLine());
+            double a = double.Parse(Console.ReadLine());
+
+            double clearHallAreaCm2 = l * w * 10000 * 0.9 - (a*a * 10000);
+            double dancers = clearHallAreaCm2 / 7040;
+
+            Console.WriteLine($"{Math.Floor(dancers)}");
+        }
+    }
+}
