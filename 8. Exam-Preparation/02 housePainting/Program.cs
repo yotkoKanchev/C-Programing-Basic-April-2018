90c79f9035db68e7a7fa5acc000f3e1ef904bc98
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-29 22:52:24 +0200

    Add files via upload

---
+++ 8. Exam-Preparation/02 housePainting/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02housePainting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double houseHight = double.Parse(Console.ReadLine());
+            double wallLenght = double.Parse(Console.ReadLine());
+            double roofHight = double.Parse(Console.ReadLine());
+
+            double wallsArea = 2 * (houseHight * houseHight + houseHight * wallLenght) - 2 * 1.2 - 2 * 1.5 * 1.5;
+            double roofArea = 2 * (wallLenght * houseHight + houseHight * roofHight / 2);
+
+            double greenPaint = wallsArea / 3.4;
+            double redPaint = roofArea / 4.3;
+
+            Console.WriteLine($"{greenPaint:f2}");
+            Console.WriteLine($"{redPaint:f2}");
+        }
+    }
+}
