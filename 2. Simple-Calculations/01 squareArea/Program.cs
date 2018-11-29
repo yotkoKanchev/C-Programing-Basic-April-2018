13e6f3614d7c58782b9b828b723dcba58933a345
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-29 19:22:24 +0200

    Add files via upload

---
+++ 2. Simple-Calculations/01 squareArea/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace squareArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+
+            double sqArea = a * a;
+            Console.Write("Square = ");
+            Console.WriteLine(sqArea);
+        }
+    }
+}
