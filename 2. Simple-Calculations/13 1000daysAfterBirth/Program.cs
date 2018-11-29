35b56a9bda72f69a8bf00c2e26bb4833d1c5db0d
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-29 22:29:27 +0200

    Add files via upload

---
+++ 2. Simple-Calculations/13 1000daysAfterBirth/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _131000daysAfterBirth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime date = Convert.ToDateTime(Console.ReadLine());
+            date = date.AddDays(999);
+            string pattern = "dd-MM-yyyy";
+            Console.WriteLine(date.ToString(pattern));
+            
+        }
+    }
+}
