61f86419f6bc0eb6518ea33889ae89c2fc6421a9
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-29 16:19:30 +0200

    Add files via upload

---
+++ 3. Simple-Conditions/09 Password Guess/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordGuess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string pasword = "s3cr3t!P@ssw0rd";
+
+                if (input == pasword)
+                Console.WriteLine("Welcome");
+
+                else
+                Console.WriteLine("Wrong password!");
+        }
+    }
+}
