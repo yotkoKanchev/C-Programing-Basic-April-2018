14a6066f8856aff2fe68498d36d1c114e3693a37
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-29 23:10:05 +0200

    Add files via upload

---
+++ 6. Drawing-with-Loops/07 Christmas Tree/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Christmas_Tree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int leftSpaces = n + 1;
+
+            string emptySpaces = new string(' ', leftSpaces);
+            Console.WriteLine(emptySpaces + "|" + emptySpaces);
+
+            leftSpaces = leftSpaces - 2;
+
+            for (int i = 0; i < n; i++)
+            {
+                emptySpaces = new string(' ', leftSpaces);
+                Console.Write(emptySpaces);
+                Console.Write(new string('*', i + 1));
+                Console.Write(" | ");
+                Console.Write(new string('*', i + 1));
+                Console.WriteLine();
+                leftSpaces--;
+            }
+        }
+    }
+}
