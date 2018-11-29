0f0b0c02525c5e7b050782c451f4721df9349217
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-29 22:41:13 +0200

    Add files via upload

---
+++ 4. Complex-Conditions-Exercises/04 fruitOrVegetable/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04fruitOrVegetable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fruit = Console.ReadLine();
+            string result = "";
+            if (fruit == "banana" || fruit == "apple" || fruit == "kiwi" || fruit == "cherry" || fruit == "lemon" || fruit == "grapes")
+            {
+                result = "fruit";
+            }
+            else if (fruit == "tomato" || fruit == "cucumber" || fruit == "pepper" || fruit == "carrot")
+            {
+                result = "vegetable";
+            }
+            else
+            {
+                result = "unknown";
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
