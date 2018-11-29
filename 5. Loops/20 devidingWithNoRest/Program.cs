b1cea176372fc0a79e2ec518af25bce2cf31bb8e
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-29 18:08:44 +0200

    Add files via upload

---
+++ 5. Loops/20 devidingWithNoRest/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20devidingWithNoRest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n = double.Parse(Console.ReadLine());
+            double p1 = 0;
+            double p2 = 0;
+            double p3 = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                double num = double.Parse(Console.ReadLine());
+
+                if (num % 2 == 0)
+                {
+                    p1 += 1;
+                }
+                if (num % 3 == 0)
+                {
+                    p2 += 1;
+                }
+                if (num % 4 == 0)
+                {
+                    p3 += 1;
+                }
+            }
+            Console.WriteLine($"{(100 / n * p1):f2}%");
+            Console.WriteLine($"{(100 / n * p2):f2}%");
+            Console.WriteLine($"{(100 / n * p3):f2}%");
+        }
+    }
+}
