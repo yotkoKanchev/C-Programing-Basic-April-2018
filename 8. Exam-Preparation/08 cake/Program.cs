f58420a5444fefb647bb6f3fa2eb424be8a9f2bd
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-29 22:55:22 +0200

    Add files via upload

---
+++ 8. Exam-Preparation/08 cake/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _04._Cake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cakeLenght = int.Parse(Console.ReadLine());
+            int cakeWidth = int.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+            int cakePieces = cakeLenght * cakeWidth;
+
+            while (input != "STOP")
+            {
+                int pieces = int.Parse(input);
+
+                cakePieces -= pieces;
+                if (cakePieces < 0)
+                {
+                    Console.WriteLine($"No more cake left! You need {Math.Abs(cakePieces)} pieces more.");
+                    return;
+                }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"{cakePieces} pieces are left.");
+        }
+    }
+}
