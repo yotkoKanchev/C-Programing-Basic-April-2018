e155e0e3366f448d3945f9bb6331ff7207a26334
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-29 18:04:07 +0200

    Add files via upload

---
+++ 5. Loops/05 maxNumber/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05maxNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int maxNum = -10000000;
+            for (int i = 1; i <= n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                if (number >= maxNum)
+                {
+                    maxNum = number;
+                }
+            }
+            Console.WriteLine(maxNum);
+        }
+    }
+}
