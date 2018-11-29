76ff23e3cfdcbd20aff6aeb0e34ec3e6c7a4eaea
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-29 22:30:54 +0200

    Add files via upload

---
+++ 2. Simple-Calculations/16 birthDay/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14birthDay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double lenght = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+            double procentidge = double.Parse(Console.ReadLine());
+
+            double volume = lenght * width * height;
+            double litters = (volume * 0.001) * (100 - procentidge)/100;
+
+            Console.WriteLine($"{litters:f3}");
+        }
+    }
+}
