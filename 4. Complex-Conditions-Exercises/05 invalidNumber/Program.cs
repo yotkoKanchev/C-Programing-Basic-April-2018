0dba97318bdd73a11a733d93287b5dd6e60deee1
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-29 22:41:43 +0200

    Add files via upload

---
+++ 4. Complex-Conditions-Exercises/05 invalidNumber/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05invalidNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            bool isValid = ((number >= 100 && number <= 200) || number == 0);
+
+            if (!isValid)
+            {
+                Console.WriteLine("invalid");
+            }
+        }
+    }
+}
