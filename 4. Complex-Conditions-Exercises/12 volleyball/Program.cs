2e7be0a00cb67625a7357f491502403c9939f620
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-29 22:45:03 +0200

    Add files via upload

---
+++ 4. Complex-Conditions-Exercises/12 volleyball/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12volleyball
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string year = Console.ReadLine();
+            double holidays = double.Parse(Console.ReadLine());
+            double weekends = double.Parse(Console.ReadLine());
+
+            double games = 0;
+            switch (year)
+            {
+                case "leap":
+                    games = 1.15 * (weekends + (48 - weekends) * 3 / 4 + holidays * 2 / 3);
+                    break;
+                case "normal":
+                    games = weekends + (48 - weekends) * 3 / 4 + holidays * 2 / 3;
+                    break;
+            }
+            Console.WriteLine($"{Math.Floor(games)}");
+        }
+    }
+}
