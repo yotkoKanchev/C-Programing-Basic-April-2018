f081d59172999fbe11d024807b4dedc387a4f8c9
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-29 16:18:17 +0200

    Add files via upload

---
+++ 3. Simple-Conditions/07 Sum seconds/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sumSeconds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstPlayer = int.Parse(Console.ReadLine());
+            int secondPlayer = int.Parse(Console.ReadLine());
+            int thirdPlayer = int.Parse(Console.ReadLine());
+
+            int totalSeconds = firstPlayer + secondPlayer + thirdPlayer;
+
+            int minutes = totalSeconds / 60;
+            int seconds = totalSeconds % 60;
+
+            if (seconds > 9)
+            {
+                Console.WriteLine($"{minutes}:{seconds}");
+            }
+
+            else
+            {
+                Console.WriteLine($"{minutes}:0{seconds}");
+            }
+
+
+        }
+    }
+}
