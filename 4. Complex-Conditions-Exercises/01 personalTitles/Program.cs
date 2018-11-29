62ede7b7d666947170b0819cb9ae9ac714c112f3
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-29 22:39:48 +0200

    Add files via upload

---
+++ 4. Complex-Conditions-Exercises/01 personalTitles/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01personalTitles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double age = double.Parse(Console.ReadLine());
+            string gender = Console.ReadLine();
+            string result = "";
+            switch (gender)
+            {
+                case "m":
+                    if (age < 16)
+                    {
+                        result = "Master";
+                    }
+                    else
+                    {
+                        result = "Mr.";
+                    }
+                    break;
+                case "f":
+                    if (age < 16)
+                    {
+                        result = "Miss";
+                    }
+                    else
+                    {
+                        result = "Ms.";
+                    }
+                    break;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
