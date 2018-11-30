91441220f3a6cb4b219bd74dae203bbee3d2ea5c
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 09:01:18 +0200

    Add files via upload

---
+++ 7. Advanced-Loops/08 factorial/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int num = 1;
+            do
+            {
+                num = num * n;
+                n--;
+            } while (n > 0);
+            Console.WriteLine(num);
+        }
+    }
+}
