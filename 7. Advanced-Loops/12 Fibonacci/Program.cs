a206f9a75faee62334099f646e0b93c7e81f23a1
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 09:03:34 +0200

    Add files via upload

---
+++ 7. Advanced-Loops/12 Fibonacci/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int fib1 = 1;
+            int fib2 = 1;
+
+            for (int i = 1; i < n; i++)
+            {
+                int fibNext = fib1 + fib2;
+                fib1 = fib2;
+                fib2 = fibNext;
+            }
+            Console.WriteLine(fib2);
+        }
+    }
+}
