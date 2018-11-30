524d3f7961f3957fee1a21d5e54ae151e4c8713c
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 13:33:35 +0200

    Add files via upload

---
+++ OnlineExam16_17June2018/04. Cat Food/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.Cat_Food1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cats = int.Parse(Console.ReadLine());
+
+            int smallCats = 0;
+            int bigCats = 0;
+            int hugeCats = 0;
+
+            double totalFoodInGrams = 0;
+
+            for (int i = 0; i < cats; i++)
+            {
+                var foodInGrams = double.Parse(Console.ReadLine());
+
+                totalFoodInGrams = totalFoodInGrams + foodInGrams;
+            
+
+                if (foodInGrams >= 100 && foodInGrams < 200)
+                {
+                    smallCats++;
+                }
+
+                else if (foodInGrams >= 200 && foodInGrams < 300)
+                {
+                    bigCats++;
+                }
+
+                else if (foodInGrams >= 300 && foodInGrams <= 400)
+                {
+                    hugeCats++;
+                }
+                
+            }
+            var totalFoodInKg = totalFoodInGrams / 1000;
+            var price = totalFoodInKg * 12.45;
+
+            Console.WriteLine($"Group 1: {smallCats} cats.");
+            Console.WriteLine($"Group 2: {bigCats} cats.");
+            Console.WriteLine($"Group 3: {hugeCats} cats.");
+            Console.WriteLine($"Price for food per day: {price:F2} lv. ");
+        }
+    }
+}
