d9661834523c9e2494b968581afd5e7a444582cf
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 08:59:26 +0200

    Add files via upload

---
+++ 7. Advanced-Loops/06 numberInRange1To100/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberInRange1To100
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            
+            while (n < 1 || n > 100)
+                
+            {
+                Console.WriteLine("Еnter a number in the range [1...100]:");
+                Console.WriteLine("Invalid number!");
+                n = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine($"The number is: {n}");
+        }
+    }
+}
