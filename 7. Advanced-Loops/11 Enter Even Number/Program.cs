821eef8a30a681083ea362f51cbb5a7d46b16184
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 09:03:08 +0200

    Add files via upload

---
+++ 7. Advanced-Loops/11 Enter Even Number/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enter_Even_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter even number:");
+
+                int num = 0;
+
+                bool isInteger = int.TryParse(Console.ReadLine(), out num);
+
+                if (isInteger)
+                {
+                    if (num % 2 == 0)
+                    {
+                        Console.WriteLine($"Even number entered: {num}");
+                        break;
+                    }
+                    Console.WriteLine("The number is not even.");
+                }
+
+                else
+                {
+                    Console.WriteLine("Invalid number!");
+                }
+            }
+        }
+    }
+}
