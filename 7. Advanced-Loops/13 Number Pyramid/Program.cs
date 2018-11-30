cf725a8ff34e68eb57e6a74dcfbd235133073f20
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 09:04:05 +0200

    Add files via upload

---
+++ 7. Advanced-Loops/13 Number Pyramid/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number_Pyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int num = 1;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write(num + " ");
+                    num++;
+                    if (num > n)
+                    {
+                        break;
+                    }
+                }
+                
+                if (num > n)
+                {
+                    break;
+                }
+                Console.WriteLine();
+            }            
+        }
+    }
+}
