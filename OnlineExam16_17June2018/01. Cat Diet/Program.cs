179a31d33df68081f15276763b70121f2a87b46b
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 13:32:20 +0200

    Add files via upload

---
+++ OnlineExam16_17June2018/01. Cat Diet/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Cat_Diet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double fats = double.Parse(Console.ReadLine());
+            double proteins = double.Parse(Console.ReadLine());
+            double carbohs = double.Parse(Console.ReadLine());
+            double calories = double.Parse(Console.ReadLine());
+            double water = double.Parse(Console.ReadLine());
+
+            double gramsFromFats = ((fats / 100) * calories) / 9;
+            double gramsFromProteins = ((proteins / 100) * calories) / 4;
+            double gramsFromCarbohs = ((carbohs / 100) * calories) / 4;
+            double totalWeight = gramsFromFats + gramsFromCarbohs + gramsFromProteins;
+            double caloriesPergram = calories / totalWeight ;
+
+            double finalCalories = caloriesPergram * (1 - (water / 100));
+
+            Console.WriteLine($"{finalCalories:F4}");
+
+
+        }
+    }
+}
